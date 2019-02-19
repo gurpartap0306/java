d6e145f82cfe2fde33bda0ddff24108d1f5cf082
gurpartap0306 <dhaliwal.gurpartap@gmail.com>
2019-02-19 11:33:40 +0530

    update GameHelper

--- GameHelper.java
+++ GameHelper.java
@@ -1,49 +1,6 @@
 import java.io.*;
 import java.util.*;
 
-
-public class DotComBust(){
-
-private GameHelper helper = new GameHelper();
-private ArrayList<DotCom> dotComslist = new ArrayList<DotCom>;
-
-public void setUpGame(){
-  //first make some dot coms and give them locatiion
-  DotCom one = new DotCom();
-  one.setName("Pets.com");
-  DotCom two = new DotCom();
-  two.setName("eToys.com");
-  DotCom three = new DotCom();
-  three.setName("Go2.com");
-  dotComslist.add(one);
-  dotComslist.add(two);
-  dotComslist.add(three);
-
-  for (DotCom dotComToSet : dotComslist) {
-    ArrayList<String> newLocation = helper.placeDotCom(3);
-    dotComToSet.setLocationCells(newLocation);
-  }//close for loop
-}//close setUpGame method
-
-  public static void main(String[] args){
-    DotComBust ob = new DotComBust();
-
-  }
-}
-
-public class DotCom(){
-  private ArrayList<String> locationCells;
-  private String name;
-
-  public void setLocationCells(ArrayList<String> loc){
-  locationCells =loc;
-  }
-
-  public Void setName(String n){
-    name = n;
-  }
-}
-
 public class GameHelper{
   private static final String alphabet = "abcdefg";
   private int gridLength = 7;
@@ -68,7 +25,7 @@ public class GameHelper{
 
     while (!success && attempts++ < 200){
       location =(int) (Math.random()*gridSize);
-      //  System.out.print("try" + location);
+        System.out.print("try" + location);
       int x =0;
           success = true;
           while(success && x < comSize){
@@ -82,7 +39,7 @@ public class GameHelper{
                 success = false;
               }
             }else{
-            //  System.out.print("used" + location);
+              System.out.print("used" + location);
               success = false;
             }
           }
@@ -90,7 +47,7 @@ public class GameHelper{
     int x = 0;
     int row =0;
     int column =0;
-    //System.out.println("\n");
+    System.out.println("\n");
     while(x < comSize){
       grid[coords[x]] = 1;
       row = (int) (coords[x] / gridLength);
@@ -99,9 +56,17 @@ public class GameHelper{
 
       alphaCells.add(temp.concat(Integer.toString(row)));
       x++;
-    //  System.out.println("  coord "+x+" = " + alphaCells.get(x-1));
+      System.out.println("  coord "+x+" = " + alphaCells.get(x-1));
     }
-    //  System.out.println("\n");
+      System.out.println("\n");
       return alphaCells;
+    }
+
+
+    public static void main (String[] args){
+      GameHelper ob =new GameHelper();
+      ArrayList<String> a= ob.placeDotCom(3);
+
+      System.out.println(a);
     }
 }
